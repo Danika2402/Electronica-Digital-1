f076233558147882fea9bcae7dc3673300c1950f
Danika2402 <and19487@uvg.edu.gt>
2020-08-16 09:01:21 -0600

    testbench
    tablas de verdad y algunos toques

--- Laboratorio_5/Module.v
+++ Laboratorio_5/Module.v
@@ -20,18 +20,18 @@ endmodule
 
 module tabla2_2a1(input wire A, B,C, output wire Y);
 
-		assign Y = A? (B~|C):(B^C);
+		assign Y = A? (B^C):(B~|C);
 
 endmodule
 
 module tabla2_4a1(input wire A,B,C, output wire Y);
 
-		assign Y = A?(B? (~C):0):(B?(C):(~C));
+		assign Y = A?(B? (~C):(C)):(B?(0):(~C));
 
 endmodule
 
 module tabla2_8a1(input wire A,B,C, output wire Y);
 		
-		assign Y = A? (B? (C? 1:0):(C? 0:0)): (B? (C? 0:1):(C? 1:0));
+		assign Y = A? (B? (C? 0:1):(C? 1:0)):(B? (C? 0:0):(C? 0:1));
 
 endmodule
--- Laboratorio_5/Module_tb.v
+++ Laboratorio_5/Module_tb.v
@@ -1,16 +1,23 @@
 module tesbench();
 
-	reg p1,p2,p3,p4,p5,p6,p7,p8,p9;
-	wire led1,led2led3;
+	reg p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18;
+	wire led1,led2,led3,led4,led5,led6;
 	
-	tabla1_2a1 mux2(p1,p2,p3,led1);
+	tabla1_2a1 T1mux2(p1,p2,p3,led1);
 	
-	tabla1_4a1 mux4(p4,p5,p6,led2);
+	tabla1_4a1 T1mux4(p4,p5,p6,led2);
 	
-	tabla1_8a1 mux8(p7,p8,p9,led3);
+	tabla1_8a1 T1mux8(p7,p8,p9,led3);
+	
+	tabla2_2a1 T2mux2(p10,p11,p12,led4);
+	
+	tabla2_4a1 T2mux4(p13,p14,p15,led5);
+	
+	tabla2_8a1 T2mux8(p16,p17,p18,led6);
  	
 	initial begin 
 	
+	$display("Tabla 1, mux 2:1");
 	$display("A B C | Y");
 	$display("------|--");
 	$monitor("%b %b %b | %b", p1,p2,p3,led1);
@@ -29,6 +36,7 @@ module tesbench();
 	
 	#20
 	$display("\n");
+	$display("Tabla 1, mux 4:1");
 	$display("A B C | Y");
 	$display("------|--");
 	$monitor("%b %b %b | %b", p4,p5,p6,led2);
@@ -47,6 +55,7 @@ module tesbench();
 	
 	#40
 	$display("\n");
+	$display("Tabla 1, mux 8:1");
 	$display("A B C | Y");
 	$display("------|--");
 	$monitor("%b %b %b | %b", p7,p8,p9,led3);
@@ -58,6 +67,63 @@ module tesbench();
 	#1 p7=1;p8=0;p9=1;
 	#1 p7=1;p8=1;p9=0;
 	#1 p7=1;p8=1;p9=1;
+	
+	end
+	
+	initial begin
+	
+	#80
+	$display("\n");
+	$display("Tabla 2, mux 2:1");
+	$display("A B C | Y");
+	$display("------|--");
+	$monitor("%b %b %b | %b", p10,p11,p12,led4);
+	p10=0;p11=0;p12=0;
+	#1 p10=0;p11=0;p12=1;
+	#1 p10=0;p11=1;p12=0;
+	#1 p10=0;p11=1;p12=1;
+	#1 p10=1;p11=0;p12=0;
+	#1 p10=1;p11=0;p12=1;
+	#1 p10=1;p11=1;p12=0;
+	#1 p10=1;p11=1;p12=1;
+	
+	end
+	
+	initial begin
+	
+	#100
+	$display("\n");
+	$display("Tabla 2, mux 4:1");
+	$display("A B C | Y");
+	$display("------|--");
+	$monitor("%b %b %b | %b", p13,p14,p15,led5);
+	p13=0;p14=0;p15=0;
+	#1 p13=0;p14=0;p15=1;
+	#1 p13=0;p14=1;p15=0;
+	#1 p13=0;p14=1;p15=1;
+	#1 p13=1;p14=0;p15=0;
+	#1 p13=1;p14=0;p15=1;
+	#1 p13=1;p14=1;p15=0;
+	#1 p13=1;p14=1;p15=1;
+	
+	end
+	
+	initial begin
+	
+	#120
+	$display("\n");
+	$display("Tabla 2, mux 8:1");
+	$display("A B C | Y");
+	$display("------|--");
+	$monitor("%b %b %b | %b", p16,p17,p18,led6);
+	p16=0;p17=0;p18=0;
+	#1 p16=0;p17=0;p18=1;
+	#1 p16=0;p17=1;p18=0;
+	#1 p16=0;p17=1;p18=1;
+	#1 p16=1;p17=0;p18=0;
+	#1 p16=1;p17=0;p18=1;
+	#1 p16=1;p17=1;p18=0;
+	#1 p16=1;p17=1;p18=1;
 	
 	end
 endmodule
